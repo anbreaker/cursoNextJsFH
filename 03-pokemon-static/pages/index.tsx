a21f998b8cb87068d809eec5b7c911ed1f833bec
anbreaker <antunez19@gmail.com>
2023-05-14 12:43:52 +0200

    feat: config NextUi and _document.tsx

--- 03-pokemon-static/pages/index.tsx
+++ 03-pokemon-static/pages/index.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next';
 
+import { Button } from '@nextui-org/react';
+
 const HomePage: NextPage = () => {
   return (
     <>
       <h1>Home: NextPage</h1>
+      <Button color={'gradient'}>click</Button>
     </>
   );
 };
