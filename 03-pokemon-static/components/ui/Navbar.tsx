d37bda08e74dc94298fd64e635bb2c311d1a76bb
anbreaker <antunez19@gmail.com>
2023-05-27 10:07:23 +0200

    fix: added nextlink wit legacy to repare refresh navbar

--- 03-pokemon-static/components/ui/Navbar.tsx
+++ 03-pokemon-static/components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from '@nextui-org/react';
 import { Spacer, Text, theme } from '@nextui-org/react';
+import NextLink from 'next/link';
 
 import navbarCss from './Navbar.module.css';
 import Image from 'next/image';
@@ -7,29 +8,33 @@ import Image from 'next/image';
 export const Navbar = () => {
   return (
     <div className={navbarCss.nav} style={{ backgroundColor: theme.colors.gray900.value }}>
-      <Link href="/">
-        <Image
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-          alt="Icon App"
-          width={70}
-          height={70}
-        />
+      <NextLink href="/" legacyBehavior passHref>
+        <Link>
+          <Image
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="Icon App"
+            width={70}
+            height={70}
+          />
 
-        <Text color="aliceblue" h2>
-          P
-        </Text>
-        <Text color="aliceblue" h3>
-          okémon
-        </Text>
-      </Link>
+          <Text color="aliceblue" h2>
+            P
+          </Text>
+          <Text color="aliceblue" h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
 
       <Spacer css={{ flex: 1 }} />
 
-      <Link href="/favorites">
-        <Text color="aliceblue" h4>
-          Favourites
-        </Text>
-      </Link>
+      <NextLink href="/favorites" legacyBehavior passHref>
+        <Link>
+          <Text color="aliceblue" h4>
+            Favourites
+          </Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
